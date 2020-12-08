bb944d1628b33337595c082e578cfcb34ad60b5a
npretli <72082122+npretli@users.noreply.github.com>
2020-12-08 10:21:01 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,3 +4,16 @@ function myFunction() {
     x.className += " responsive";
   } 
 }
+
+/*jquery allows folders to be opened when clicked on*/
+$(document).ready(function(){
+        $("#trailtitle1").click(function(){
+            $("#trailtext1").fadeToggle();
+        });
+    });
+    $(document).ready(function(){
+        $("#trailtitle2").click(function(){
+            $("#trailtext2").fadeToggle();
+        });
+    });
+</script>
